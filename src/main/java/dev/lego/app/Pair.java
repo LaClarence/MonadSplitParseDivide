f3ed4787694f641b2dd231ad21eae3df6225792c
Lego <LaClarence@users.noreply.github.com>
2025-05-16 16:33:29 +0200

    Exclude null value on Pair record

--- src/main/java/dev/lego/app/Pair.java
+++ src/main/java/dev/lego/app/Pair.java
@@ -1,4 +1,9 @@
 package dev.lego.app;
 
 record Pair<T>(T _1, T _2) {
+  Pair {
+    if (_1 == null || _2 == null) {
+      throw new IllegalArgumentException("Pair elements cannot be null");
+    }
+  }
 }
